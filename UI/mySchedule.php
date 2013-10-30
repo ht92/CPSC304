d6d3625f5cf22c06ba23c43ba245214a0aaeddb5
Diana Sutandie <dianasutandie@gmail.com>
2013-10-29 18:26:45 -0700

    updated MySchedule with dummy data

--- UI/mySchedule.php
+++ UI/mySchedule.php
@@ -10,27 +10,8 @@
 	<div id="main">
 	<?php include("headerLogo.php"); ?>
 
-	<h2> Find Order </h2>
-	<table border="0">
-  		<tr>
-    		<td>Order ID</td>
-    		<td><form action=""><input type="text" name="orderID"></form></td>
-    		<td><input type="submit" value="submit"></td>
-  		</tr>
-  		<tr>
-  			<td>Made for Date </td>
-  			<td><form action=""><input type="date" name="madeForDate"></form></td>
-  			<td><input type="submit" value="submit"></td>
-  		</tr>
-	</table>
-
-
-	<h2> Incomplete Order </h2>
+	<h2> My Schedule </h2>
 	<?php include("staff_incompleteOrderTable.php"); ?>
-
-	<h2> Complete Order </h2>
-	<?php include("staff_completeOrderTable.php"); ?>
-
 
 	<?php include("Footer.php"); ?>
 </div>
