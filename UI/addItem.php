773f79e857049d569ed6d64e3d7fbe18690ad017
joeyMac <joey.mac@outlook.com>
2013-11-26 03:44:49 -0800

    Users can now manipulate item data

--- UI/addItem.php
+++ UI/addItem.php
@@ -9,7 +9,25 @@
 <?php include("headerStaff.php"); ?>
 	<div id="main">
 	<?php include("headerLogo.php"); ?>
-	<?php include "utility.php"; ?>
+	<?php include "utility.php"; 
+        if(isset($_POST['submitted']))
+        {
+           $itemID = str_pad($_POST['itemID'], 5, "0", STR_PAD_LEFT);
+           $query = "insert into item values('" . $itemID . "', '"
+                    . $_POST['itemName'] . "', '" . $_POST['itemType'] .
+                    "', " . $_POST['price'] . ")";
+           oci_free_statement(executeCommand($query));
+           if(!$status)
+           {
+              echo "<br>Error adding Item to DataBase<br>";
+           }
+           else
+           {
+              OCICommit($dbHandle);
+           }
+        }
+        OCILogoff($dbHandle);  
+             ?>
 	
 	<form name"TaskAdded" method="post" action="">
 	<table border="0">
@@ -18,9 +36,17 @@
         <td><input type="text" name="itemID"></td>
       </tr>
       <tr>
-        <td>Quantity: </td>
-        <td><input type="number" name="quantity"></td>
+        <td>Name:</td>
+        <td><input type="text" name="itemName"></td>
       </tr>
+      <tr>
+         <td>Type:</td>
+         <td><input type='text' name='itemType'></td>
+      </tr>
+      <tr>
+         <td>Price</td>
+         <td><input type='float' name='price'></td>
+      </tr>  
       <tr>
         <td>
             <input type="submit" name='submitted'>
