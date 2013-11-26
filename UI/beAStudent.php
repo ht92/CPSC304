2f7f9c05b23c66b972bb89fc1d694f37b0a53ea5
joeyMac <joey.mac@outlook.com>
2013-11-25 22:46:02 -0800

    Finished option for user to become a student

--- UI/beAStudent.php
+++ UI/beAStudent.php
@@ -5,7 +5,27 @@
 </head>
 <body>
 
-	<form method="post" action="">
+<?php
+if(isset($_POST['submit']))
+{
+
+   $addStudent = "insert into Student values('" . $userID . "')";
+   oci_free_statement(executeCommand($addStudent));
+   if($status)
+   {
+      OCICommit($dbHandle);
+      $isStudent = true;
+      $isStudentURL = "true";
+      header("Location: classes.php?isMember=" . $isMemberURL . "&userID=" .
+             $userID . "&isStudent=" . $isStudentURL);
+   }
+   else
+   {
+      echo "<br>Error enrolling user as a student<br>";
+   }
+}
+?>
+	<form name="enroll" method="post" action="">
 		<input type="submit" name="submit" value="Register as A Student" />
 	</form>
 
