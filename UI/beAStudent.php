4778fa22cfc952f59bc90e54c1d351372a290dd4
Diana Sutandie <dianasutandie@gmail.com>
2013-11-25 22:30:36 -0800

    Add the functionality to be a student and different views for student and non-student

---
+++ UI/beAStudent.php
@@ -0,0 +1,14 @@
+<html>
+<head>
+	<title>Become a student</title>
+	<link href="Site.css" rel="stylesheet">
+</head>
+<body>
+
+	<form method="post" action="">
+		<input type="submit" name="submit" value="Register as A Student" />
+	</form>
+
+
+</body>
+</html>
