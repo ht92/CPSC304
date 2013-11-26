99eff65adfe865cf62fd436484d7e2af633cbefa
Reinhard Simeon <r31n4ard@gmail.com>
2013-11-25 19:52:07 -0800

    account.php is edited

--- UI/account.php
+++ UI/account.php
@@ -1,8 +1,17 @@
 <!DOCTYPE html>
 <html>
 <head>
-<title>Bakerzin-staff</title>
+<title>Bakerzin-Customer</title>
 <link href="Site.css" rel="stylesheet">
+<script>
+function disableElements()
+{
+document.getElementById("name").disabled=false;
+document.getElementById("address").disabled=false;
+document.getElementById("phone").disabled=false;
+document.getElementById("id").disabled=false;
+}
+</script>
 </head>
 
 <body>
@@ -11,18 +20,17 @@
 	<h1>Make Order</h1> 
 	<p>Please fill the following</p>
 
-	<table border="1">
-		<form action="account.php">
-			<tr>Name:</tr><input type="text" name="email" size="35" disabled value="VARIABLE FOR QUERY"><br>
-			<tr>Address:</tr><input type="text" name="pin" size="35" disabled value="VARIABLE FOR QUERY"><br>
-			<tr>Phone Number:</tr><input type="text" name="phone" size="35" disabled value="VARIABLE FOR QUERY"><br>
-			<tr>Customer ID:</tr><input type="text" name="id" size="35" disabled value="VARIABLE FOR QUERY"><br>
-			<! add more attributes here and make query to retrieve information>
-			<input type="submit" value="Edit">
+		<form action="account.php" name="account">
+			Name:<input type="text" id="name" size="35" value="VARIABLE FOR QUERY" disabled><br>
+			Address:<input type="text" id="address" size="35" value="VARIABLE FOR QUERY" disabled><br>
+			Phone Number:<input type="text" id="phone" size="35" value="VARIABLE FOR QUERY" disabled><br>
+			Customer ID:<input type="text" id="id" size="35" value="VARIABLE FOR QUERY" disabled><br>			
+			<input type="submit" id="submit" value="Submit">
 		</form>
+		<button onclick="disableElements()">Edit Information</button>
 	<?php include("Footer.php") ?>
 	</div>
-	</table>
+
 
 </body>
 </html>
